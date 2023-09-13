873606801ab27c69a454da3f853b0d5ffb6c4334
Deni <dendi_maksimof@mail.ru>
2023-09-13 17:45:22 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const cols = document.querySelectorAll('.col')
 
+document.addEventListener('keydown', event => {
+if (event.code === 'Space') {
+    setRandomColors()
+}
+})
+
 function generateRandomColor() {
 
  const hexCodes = '0123456789ABCDEF'   
@@ -13,11 +19,20 @@ let color = ''
 function setRandomColors() {
     cols.forEach((col) => {
         const text = col.querySelector('h2')
+        const button = col.querySelector('button')
         const color = chroma.random()
 
         text.textContent = color
-       col.style.background = generateRandomColor()
+       col.style.background = color
+
+       setTextColor(text, color)
+       setTextColor(button, color)
     })
+}
+
+function setTextColor (text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.7 ? 'black' : 'white'
 }
 
 setRandomColors()
